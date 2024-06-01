96d999d2cb2d8b22f266d9f6a730b091fcc7b57f
Kushtarbek.Zhaniev <zhkushtarbek@gmail.com>
2024-05-31 23:03:59 -0400

    Apex and LWC

--- force-app/main/default/lwc/kushtarCancelChangeEnrollment/kushtarCancelChangeEnrollment.js
+++ force-app/main/default/lwc/kushtarCancelChangeEnrollment/kushtarCancelChangeEnrollment.js
@@ -1,15 +1,38 @@
-import { LightningElement } from 'lwc';
-
+import { LightningElement, api, wire } from 'lwc';
+import accountDetails from '@salesforce/apex/AccountService.getAccountDetails';
 export default class ButtonBasic extends LightningElement {
-    clickedButtonLabel;
+    //Get RecordId from URL
+    @api recordId;
+
     showModal=false;
     typeSelection=true;
     dateSelection=false;
     changeAuthorizationPage=false;
     updateButton=false;
-    submitButton=false;
+    submitButtonChange=false;
     changeConfirmationPage=false;
     cancelOrCloseButton='Cancel';
+    cancelAuthorizationPage=false;
+    submitButtonCancel=false;
+    cancelConfirmationPage=false;
+
+    //Variable from Apex
+    ExternalAccountId;
+    ServiceStartDate
+    ServiceEndDate
+    ServiceAddress
+
+
+    @wire(accountDetails, { AccountId: '$recordId' })
+    accountDetailsResponse({ error, data }) {
+        if (data) {
+            console.log(data);
+            this.ExternalAccountId = data.ExternalAccountId;
+        }
+        if(error){
+            console.log(error);
+        }
+    }
 
     handleClick() {
         this.showModal=true;
@@ -23,24 +46,45 @@ export default class ButtonBasic extends LightningElement {
 
     handleCancelEnrollment(){
         this.typeSelection=false;
+        this.cancelAuthorizationPage=true;
+        this.submitButtonCancel=true;
     }
 
     handleUpdate(){
         this.dateSelection=false;
         this.changeAuthorizationPage=true;
         this.updateButton=false;
-        this.submitButton=true;
+        this.submitButtonChange=true;
     }
 
-    handleSubmit(){
+    handleSubmitChange(){
         this.changeAuthorizationPage=false;
         this.changeConfirmationPage=true;
-        this.submitButton=false;
+        this.submitButtonChange=false;
         this.cancelOrCloseButton='Close';
+    }
+
+    handleSubmitCancel(){
+        this.cancelAuthorizationPage=false;
+        this.submitButtonCancel=false;
+        this.cancelOrCloseButton='Close';
+        this.cancelConfirmationPage=true;
+
     }
 
     handleClose(){
         this.showModal=false;
+        this.showModal=false;
+        this.typeSelection=true;
+        this.dateSelection=false;
+        this.changeAuthorizationPage=false;
+        this.updateButton=false;
+        this.submitButtonChange=false;
+        this.changeConfirmationPage=false;
+        this.cancelOrCloseButton='Cancel';
+        this.cancelAuthorizationPage=false;
+        this.submitButtonCancel=false;
+        this.cancelConfirmationPage=false;
     }
 
 
